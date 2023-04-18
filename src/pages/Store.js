4085f3b07d8e2b8ee131580b955c1e4b13509a1f
NYaCodes <107141629+NYaCodes@users.noreply.github.com>
2023-04-18 15:41:08 -0500

    Creating a modal for the cart

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,18 +1,20 @@
 import {Row, Col} from 'react-bootstrap';
-
-
+import { productsArray } from '../productsStore';
+import ProductCard from '../components/ProductCard';
 
 function Store() {
     return(
         <>
-        <h1>Welcome to the store!</h1>
+        <h1 align="center" className='p-3'>Welcome to the store!</h1>
         <Row xs={1} md={3} className="g-4" >
-            <Col align="center">
-                <h1> Product</h1>
+            {productsArray.map((product, idx) => (
+                <Col align="center" key={idx}>
+                <h1> {product.title}</h1>
+                <ProductCard product={product} />
             </Col>
-             <Col align="center">
-                <h1> Product</h1>
-            </Col>
+            ))}
+            
+             
         </Row>
         </>
     )
