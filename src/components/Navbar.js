421d8ad8caf328b88c85358a62b695b3bb20c64b
NYaCodes <107141629+NYaCodes@users.noreply.github.com>
2023-04-18 16:43:51 -0500

    Showing cart data in modal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
 import{Button, Container, Navbar, Modal} from 'react-bootstrap'
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { CartContext } from '../CartContext';
 
 function NavbarComponet() {
+const cart = useContext(CartContext);
+
+
     const [show, setShow] = useState(false);
 const handleclose = () => setShow(false);
 const handleShow = () => setShow(true);
+
+const productsCount = cart.items.reduce((sum, product) => sum + product.quantity, 0);
 
 return (
     <>
@@ -12,7 +18,7 @@ return (
 <Navbar.Brand href='/'>Ecommerce Store</Navbar.Brand>
 <Navbar.Toggle />
 <Navbar.Collapse className='justify-content-end'>
-<Button onClick={handleShow}> Cart 0 Items</Button>
+<Button onClick={handleShow}> Cart ({productsCount}) Items</Button>
 </Navbar.Collapse>
     </Navbar>
     <Modal show={show} onHide={handleclose}>
