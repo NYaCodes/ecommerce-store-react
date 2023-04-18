2719480ea69acaefcd679f76f8613f18b8ecbdde
NYaCodes <107141629+NYaCodes@users.noreply.github.com>
2023-04-18 14:50:22 -0500

    Creating products for React use

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import NavbarComponet from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {Container} from 'react-bootstrap';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Cancel from './pages/Cancel';
+import Success from './pages/Success';
+import Store from './pages/Store';
 function App() {
   return (
+    <Container>
     <NavbarComponet>
 
     </NavbarComponet>
+    <BrowserRouter>
+    <Routes>
+      <Route index element={<Store />}/>
+      <Route path="success" element={<Success />}/>
+      <Route path="cancel" element={<Cancel />} />
+    </Routes>
+    </BrowserRouter>
+    </Container>
   );
 }
 
