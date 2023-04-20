63a0e9a755d4a23bf48ef4f4f529ddcb0d3e3836
NYaCodes <107141629+NYaCodes@users.noreply.github.com>
2023-04-19 20:16:03 -0500

    Updated Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,53 +1,52 @@
-import{Button, Container, Navbar, Modal} from 'react-bootstrap'
+import {Button, Container, Navbar, Modal} from 'react-bootstrap';
 import { useState, useContext } from 'react';
-import { CartContext } from '../CartContext';
+import { CartContext } from "../CartContext";
 import CartProduct from './CartProduct';
 
-function NavbarComponet() {
-const cart = useContext(CartContext);
-
+function NavbarComponent() {
+    const cart = useContext(CartContext);
 
     const [show, setShow] = useState(false);
-const handleclose = () => setShow(false);
-const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-const checkout = async () => {
-    await fetch('http://localhost:4000/checkout', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json'
-     },
-        body: JSON.stringify({items: cart.items})
-    }).then((response) => {
+    const checkout = async () => {
+        await fetch('http://localhost:4000/checkout', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({items: cart.items})
+        }).then((response) => {
             return response.json();
         }).then((response) => {
             if(response.url) {
                 window.location.assign(response.url); // Forwarding user to Stripe
             }
         });
-}
+    }
 
-const productsCount = cart.items.reduce((sum, product) => sum + product.quantity, 0);
+    const productsCount = cart.items.reduce((sum, product) => sum + product.quantity, 0);
 
-return (
-    <>
-    <Navbar expand="sm">
-<Navbar.Brand href='/'>Ecommerce Store</Navbar.Brand>
-<Navbar.Toggle />
-<Navbar.Collapse className='justify-content-end'>
-<Button onClick={handleShow}> Cart ({productsCount}) Items</Button>
-</Navbar.Collapse>
-    </Navbar>
-    <Modal show={show} onHide={handleclose}>
-        <Modal.Header closeButton>
-            <Modal.Title>Shopping Cart</Modal.Title>
-        </Modal.Header>
-        <Modal.Body > 
-            {productsCount > 0 ? 
-            <>
-            <p>Items in your cart</p>
-             {cart.items.map( (currentProduct, idx) => (
-                
-                <CartProduct key={idx} id={currentProduct.id} quantity={currentProduct.quantity}></CartProduct>
+    return (
+        <>
+            <Navbar expand="sm">
+                <Navbar.Brand href="/">Ecommerce Store</Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                    <Button onClick={handleShow}>Cart ({productsCount} Items)</Button>
+                </Navbar.Collapse>
+            </Navbar>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Shopping Cart</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {productsCount > 0 ?
+                        <>
+                            <p>Items in your cart:</p>
+                            {cart.items.map( (currentProduct, idx) => (
+                                <CartProduct key={idx} id={currentProduct.id} quantity={currentProduct.quantity}></CartProduct>
                             ))}
 
                             <h1>Total: {cart.getTotalCost().toFixed(2)}</h1>
@@ -59,9 +58,10 @@ return (
                     :
                         <h1>There are no items in your cart!</h1>
                     }
-            </Modal.Body>
-    </Modal>
-    </>
-)
+                </Modal.Body>
+            </Modal>
+        </>
+    )
 }
-export default NavbarComponet;
+
+export default NavbarComponent;
